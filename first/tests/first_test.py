f4177c7f88bbb2f5c1a41dfa5fadfca9b7f6ebfd
Omer Ali <79196757+Omer0191@users.noreply.github.com>
2025-06-02 15:51:55 +0000

    test: I am testing unit test for greeting

--- first/tests/first_test.py
+++ first/tests/first_test.py
@@ -1,5 +1,22 @@
 from first.first import greeting
 
+def test_greeting():
+    assert greeting("Omer", "English") == "Hello Omer!"
+
+def test_greeting1():
+    assert greeting("Omer", "Norwegian") == "Hello Omer!"
+
+def test_greeting2():
+    assert greeting("Omer", 76589) == "Hello Omer!"
+
+def test_greeting3():
+    assert greeting(Norwegian, "English") == "Hello Dani!"
+
+def test_greeting4():
+    assert greeting("Omer", "English") == "Hello Omer!"
+
+
+
 # write unit tests for the function greeting(name, language) in first/first.py
 # include different test cases by writing different unit tests
 # their names should reflect the test case, e.g. test_greeting_eng() if english is selected
